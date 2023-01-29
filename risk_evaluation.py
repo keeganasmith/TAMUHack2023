1297d1eddb7f1c573bf293800f2dfbfdd51b5df2
willheeney03 <99232572+willheeney03@users.noreply.github.com>
2023-01-28 22:53:46 -0600

    Update risk_evaluation.py

--- risk_evaluation.py
+++ risk_evaluation.py
@@ -5,9 +5,6 @@ import keys
 api_url = 'https://api.api-ninjas.com/v1/country?name=United States'
 response = requests.get(api_url, headers={'X-Api-Key': keys.ninja_api_key_will}).json()
 
-
-print(response[0]['unemployment'])
-
 unemployment_rate = response[0]['unemployment']/100
 
 print(unemployment_rate)
